f49abfd15edfedea604bb7a2b15cc22c93e214d1
Artem <artem@gmail.com>
2020-10-24 23:03:11 +0300

    Create the ability to convert an audio file from one format into other

--- app.py
+++ app.py
@@ -1,11 +1,53 @@
-from flask import Flask
+import time, random, sox, re, os
+from flask import Flask, send_from_directory, abort
+from flask import request
+from config import *
 
-DATABASE = 'sqlite3.db'
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = MAX_CONTENT_SIZE
 
-@app.route("/")
-def hello():
-    return "Hello Flask!"
+
+def get_format(file):
+    return '.' + re.split('\.', file.filename)[-1].lower()
+
+
+def is_valid_format(format):
+    return format.lower() in VALID_FORMATS
+
+
+def save_file(file):
+    format = get_format(file)
+    if is_valid_format(format):
+        try:
+            filename = str(round(time.time() * 1000)) + str(random.randint(1, 10000))
+            file.save(INPUT_DIRECTORY + filename + format)
+        except Exception as e:
+            return False,str(e)
+        return True, filename
+    return False, ''
+
+
+@app.route("/convert/<string:new_format>", methods=['POST'])
+def convert(new_format):
+    try:
+        file = request.files['file']
+        format = get_format(file)
+    except Exception as e:
+        print(e)
+        return abort(400, e)
+    if is_valid_format(format) and is_valid_format(new_format):
+        is_success, filename = save_file(file)
+        if is_success:
+            try:
+                sox.Transformer().build_file(INPUT_DIRECTORY + filename + format,
+                                             OUTPUT_DIRECTORY + filename + new_format)
+            except Exception as e:
+                print(e)
+                return abort(500, e)
+            return send_from_directory(OUTPUT_DIRECTORY, filename + new_format)
+        return abort(500, 'File can\'t be save')
+    return abort(400, 'Check formats, available formats: ' + str(VALID_FORMATS))
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,8 @@
+
+#----------------Values constants----------------
+
+DATABASE = 'sqlite3.db'
+VALID_FORMATS = ['.aif', '.aifc', '.aiff', '.au', '.avr', '.flac', '.ogg', '.snd', '.wav']
+INPUT_DIRECTORY = 'input files/'
+OUTPUT_DIRECTORY = 'output files/'
+MAX_CONTENT_SIZE = 20 * 1024 * 1024  # 20MB max limit.
