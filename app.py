2b92fe2c3510658233d9e8788a10e9d26b93b378
Bogdan Diakonenko <scrall1008@gmail.com>
2020-10-28 14:12:12 +0200

    added chorus effect

--- app.py
+++ app.py
@@ -27,6 +27,32 @@ def save_file(file):
     return False, ''
 
 
+@app.route("/chorus/<int:number_of_voices>", methods=['POST'])
+def chorus(number_of_voices):
+    try:
+        file = request.files['file']
+        format = get_format(file)
+    except Exception as e:
+        print(e)
+        return abort(400, e)
+    if number_of_voices > MAX_VOICES:
+        return abort(400, 'Too many voices, limit ' + str(MAX_VOICES))
+    if is_valid_format(format):
+        is_success, filename = save_file(file)
+        if is_success:
+            try:
+                tr = sox.Transformer()
+                tr.chorus(n_voices=number_of_voices)
+                tr.build_file(INPUT_DIRECTORY + filename + format,
+                              OUTPUT_DIRECTORY + filename + format)
+            except Exception as e:
+                print(e)
+                return abort(500, e)
+            return send_from_directory(OUTPUT_DIRECTORY, filename + format)
+        return abort(500, 'File can\'t be saved')
+    return abort(400, 'Check formats, available formats: ' + str(VALID_FORMATS))
+
+
 @app.route("/speed/<float:new_speed>", methods=['POST'])
 def speed(new_speed):
     try:
